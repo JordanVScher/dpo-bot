a87a36428c787bc2c60dfc7fa0bf03038837e237
Jordan Victor Scher <jordanvscher@hotmail.com>
2019-08-09 12:08:37 -0300

    initial commit

--- sample-run-container.sh
+++ sample-run-container.sh
@@ -2,8 +2,8 @@
 
 # arquivo de exemplo para iniciar o container
 #export SOURCE_DIR='/home/appcivico/projects/De-Olho-Nas-Metas-2017'
-export SOURCE_DIR='/home/lucas-eokoe/projects/MandatoAberto-chatbot'
-export DATA_DIR='/tmp/mandatoaberto-chatbot/data/'
+export SOURCE_DIR='/home/projects/dpo-bot'
+export DATA_DIR='/tmp/dpo-bot/data/'
 
 # confira o seu ip usando ifconfig docker0|grep 'inet addr:'
 export DOCKER_LAN_IP=$(ifconfig docker0 | grep 'inet addr:' | awk '{ split($2,a,":"); print a[2] }')
@@ -15,4 +15,4 @@ docker run --name mandatoaberto-chatbot \
  -v $SOURCE_DIR:/src -v $DATA_DIR:/data \
  -p $DOCKER_LAN_IP:$LISTEN_PORT:2049 \
  --cpu-shares=512 \
- --memory 1800m -dit --restart unless-stopped appcivico/mandatoaberto-chatbot
+ --memory 1800m -dit --restart unless-stopped appcivico/dpo-bot
