bada5e4f1ce78da93c6901e560d53faa35852fc5
JordanVScher <jordanvscher@hotmail.com>
2019-11-14 11:39:33 -0300

    Update sendAnswer

--- app/utils/sendAnswer.js
+++ app/utils/sendAnswer.js
@@ -1,19 +1,16 @@
-const MaAPI = require('../chatbot_api');
+const assistenteAPI = require('../chatbot_api');
 const { createIssue } = require('./send_issue');
 const help = require('./helper');
 
-
 async function sendAnswer(context) { // send answer from posicionamento
-	// await context.setState({ currentTheme: await context.state.knowledge.knowledge_base.find(x => x.type === 'posicionamento') });
 	await context.typingOn();
 	await context.setState({ currentTheme: context.state.knowledge.knowledge_base[0] });
 
-	// console.log('currentTheme', currentTheme);
 	if (context.state.currentTheme && ((context.state.currentTheme.answer && context.state.currentTheme.answer.length > 0)
 	|| (context.state.currentTheme.saved_attachment_type !== null && context.state.currentTheme.saved_attachment_id !== null))) {
 		try {
-			await MaAPI.setIntentStatus(context.state.politicianData.user_id, context.session.user.id, context.state.currentIntent, 1);
-			await MaAPI.logAskedEntity(context.session.user.id, context.state.politicianData.user_id, context.state.currentTheme.entities[0].id);
+			await assistenteAPI.setIntentStatus(context.state.politicianData.user_id, context.session.user.id, context.state.currentIntent, 1);
+			await assistenteAPI.logAskedEntity(context.session.user.id, context.state.politicianData.user_id, context.state.currentTheme.entities[0].id);
 
 			await help.sendTextAnswer(context, context.state.currentTheme);
 			await help.sendAttachment(context, context.state.currentTheme);
@@ -25,7 +22,7 @@ async function sendAnswer(context) { // send answer from posicionamento
 			});
 		}
 		await context.typingOff();
-	} else { // in case there's an error
+	} else {
 		await createIssue(context);
 	}
 }
