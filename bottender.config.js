5a3566cb2f8789482147a7d708dd178b4ee4dc69
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-05-14 11:50:48 -0300

    Add telegram conf

--- bottender.config.js
+++ bottender.config.js
@@ -12,13 +12,18 @@ const mapPageToAccessToken = async (pageId) => {
 
 module.exports = {
 	channels: {
-		whatsapp: {
-			enabled: true,
-			path: '/webhooks/whatsapp',
-			accountSid: process.env.ACCOUNT_SID,
-			authToken: process.env.AUTH_TOKEN,
-			phoneNumber: process.env.PHONE_NUMBER,
-		},
+		// whatsapp: {
+		// 	enabled: true,
+		// 	path: '/webhooks/whatsapp',
+		// 	accountSid: process.env.ACCOUNT_SID,
+		// 	authToken: process.env.AUTH_TOKEN,
+		// 	phoneNumber: process.env.PHONE_NUMBER,
+		// },
+		// telegram: {
+			// enabled: true,
+			// path: '/webhooks/telegram',
+			// accessToken: process.env.TELEGRAM_ACCESS_TOKEN,
+		// },
 		messenger: {
 			enabled: true,
 			path: '/webhooks/messenger',
