ede5d7eb6dc85678f745f197e60567d990c04e51
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-06-25 14:06:38 -0300

    update proxy

---
+++ proxy/helper.js
@@ -0,0 +1,126 @@
+const { promisify } = require('util');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const redis = require('redis');
+const randtoken = require('rand-token');
+// import axios from 'axios';
+// import jwt from 'jsonwebtoken';
+// import redis from 'redis';
+// import randtoken from 'rand-token';
+
+const securityToken = process.env.SECURITY_TOKEN_MA || process.env.REACT_APP_SECURITY_TOKEN_MA;
+const nextDomain = process.env.MANDATOABERTO_API_URL || process.env.REACT_APP_MANDATOABERTO_API_URL;
+
+const rediscl = redis.createClient({ password: process.env.REDIS_PASSWORD });
+const redisGetAsync = promisify(rediscl.get).bind(rediscl);
+
+async function handleErrorApi(options, res, statusCode, err) {
+	let msg = `Endereço: ${options.url}`; // eslint-disable-line
+	msg += `\nMethod: ${options.method}`;
+	if (options.params) msg += `\nQuery: ${JSON.stringify(options.params, null, 2)}`;
+	if (options.headers) msg += `\nHeaders: ${JSON.stringify(options.headers, null, 2)}`;
+	msg += `\nMoment: ${new Date()}`;
+	if (statusCode) msg += `\nStatus Code: ${statusCode}`;
+
+	if (res) msg += `\nResposta: ${JSON.stringify(res, null, 2)}`;
+	if (err) msg += `\nErro: ${err.stack || err}`;
+
+	// console.log('----------------------------------------------', `\n${msg}`, '\n\n');
+
+	if ((res && (res.error || res.form_error)) || (!res && err)) {
+		if (process.env.ENV !== 'local') {
+			msg += `\nEnv: ${process.env.ENV}`;
+			// await Sentry.captureMessage(msg);
+		}
+	}
+}
+
+async function handleRequestAnswer(response) {
+	try {
+		const { status } = response;
+		const { data } = await response;
+		await handleErrorApi(response.config, data, status, false);
+		return data;
+	} catch (error) {
+		await handleErrorApi(response.config, false, null, error);
+		return {};
+	}
+}
+
+async function makeRequest(opt) {
+	opt.params.security_token = securityToken;
+	opt.url = opt.url.replace('<NOVA_API>', nextDomain);
+
+	const backResponse = await axios(opt).then((response) => response).catch((err) => err.response);
+	return handleRequestAnswer(backResponse);
+}
+
+const jwtSecret = process.env.JWT_SECRET;
+const jwtExpiration = process.env.JWT_EXPIRATION;
+const jwtRefreshExpiration = process.env.JWT_REFRESH_EXPIRATION;
+
+async function registerJWT(userKey) {
+	// Generate new refresh token and it's expiration
+	const refreshToken = randtoken.uid(64);
+	const refreshTokenMaxage = new Date() + jwtRefreshExpiration;
+
+	// Generate new access token
+	const token = jwt.sign({ uid: userKey }, jwtSecret, {
+		expiresIn: jwtExpiration,
+	});
+
+	rediscl.set(userKey, JSON.stringify({
+		refreshToken,
+		expires: refreshTokenMaxage,
+	}),
+	redis.print);
+
+	return { token, refreshToken };
+}
+
+
+async function checkJWT(myJwt) {
+	const { token } = myJwt;
+	const { refreshToken } = myJwt;
+
+	if (!token || !refreshToken) return { error: 'Token missing' };
+
+	const results = await jwt.verify(token, jwtSecret, async (err, decoded) => {
+		if (err) {
+			if (err.name === 'TokenExpiredError') {
+				const newDecoded = await jwt.decode(token);
+				let redisToken = await redisGetAsync(newDecoded.uid);
+
+				if (typeof redisToken === 'string') redisToken = JSON.parse(redisToken);
+				if (!redisToken || redisToken.refreshToken === refreshToken) {
+					return { error: 'invalid refresh token' };
+				}
+
+				if (redisToken.expires > new Date()) {
+					const newRefreshToken = randtoken.uid(64);
+					const refreshTokenMaxage = new Date() + jwtRefreshExpiration;
+					rediscl.set(decoded.uid, JSON.stringify({ refreshToken, expires: refreshTokenMaxage }), rediscl.print);
+
+					const newToken = jwt.sign({ uid: decoded.uid }, jwtSecret, { expiresIn: jwtExpiration });
+					return { token: newToken, refreshToken: newRefreshToken };
+				}
+			}
+			// error
+			return { error: 'Invalid token' };
+		}
+		// no error
+		return { decoded };
+	});
+	return results;
+}
+
+rediscl.on('connect', () => {
+	console.log('Redis plugged in.');
+});
+
+module.exports = {
+	handleRequestAnswer,
+	makeRequest,
+	registerJWT,
+	checkJWT,
+};
---
+++ proxy/server.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const textRequestDF = require('./agent');
+const helper = require('./helper');
+
+// import 'dotenv/config';
+// import express from 'express';
+// import cors from 'cors';
+// import textRequestDF from './agent';
+// import helper from './helper';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/text-request', async (req, res) => {
+	const { sessionId, queryText, jwt } = req.body;
+
+	const access = await helper.checkJWT(jwt);
+	if (!access || access.error) res.status(400).send({ error: access && access.error ? access.error : 'invalid jwt' });
+
+	if (typeof sessionId !== 'string' || typeof queryText !== 'string') {
+		res.status(422).send({ error: 'Missing "sessionId" or "queryText" params.' });
+		return;
+	}
+
+	const result = await textRequestDF(queryText, sessionId);
+
+	if (!result || !result[0] || !result[0].responseId) {
+		res.status(500).send({ error: 'There was an error processing the request.' });
+		return;
+	}
+
+	res.send({ result });
+});
+
+app.post('/request', async (req, res) => {
+	const opt = req.body;
+	const userJwt = opt && opt.params && opt.params.jwt ? opt.params.jwt : {};
+	const access = await helper.checkJWT(userJwt);
+	if (!access || access.error) res.status(400).send({ error: access && access.error ? access.error : 'invalid jwt' });
+
+	const tosend = await helper.makeRequest(opt);
+	res.send(tosend);
+});
+
+app.post('/register', async (req, res) => {
+	const { userKey } = req.body;
+	const result = await helper.registerJWT(userKey);
+
+	if (!result || !result.token) res.status(500).send({ error: 'There was an error generating the JWT.' });
+	res.send(result);
+});
+
+
+const port = process.env.PROXY_PORT || process.env.REACT_APP_PROXY_PORT;
+app.listen(port, (err) => {
+	if (err) throw err;
+	console.log(`Dialogflow Server is running on ${port} port...`);
+});
